1a67f2773ee3a34f98d5e9a275e640260212faf1
Freddy Drennan <drennanfreddy@gmail.com>
2020-03-22 09:11:51 -0600

    update

---
+++ src/components/ui/Header.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
+const Header = (props) => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="h3" color="secondary">
+              Arc Development
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,27 @@
+// Default Theme Information: https://material-ui.com/customization/default-theme/
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
+
+const theme = createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+  typography: {
+    h3: {
+      fontWeight: 300,
+    },
+  },
+});
+
+export default theme;
